5796a64ae9a366d5b40dae43681a08dc798804e1
UditKulkarni10 <43937463+UditKulkarni10@users.noreply.github.com>
2019-10-17 15:14:57 -0400

    Validating account creation entries

--- app/src/main/java/com/example/clinic/createLogin.java
+++ app/src/main/java/com/example/clinic/createLogin.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.content.Intent;
+import android.widget.Toast;
 
 public class createLogin extends AppCompatActivity {
     private EditText Name, role, username, pwd, confpwd;
@@ -22,6 +23,7 @@ public class createLogin extends AppCompatActivity {
         create.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                validate();
                 startActivity( new Intent(createLogin.this, Login.class));
             }
         });
@@ -33,6 +35,24 @@ public class createLogin extends AppCompatActivity {
         username = (EditText)findViewById(R.id.username);
         pwd = (EditText)findViewById(R.id.pwd);
         confpwd = (EditText)findViewById(R.id.confpwd);
+    }
+
+
+    private Boolean validate(){
+        
+        String nameVal = Name.getText().toString();
+        String roleVal = role.getText().toString();
+        String usernameVal = username.getText().toString();
+        String pwdVal = pwd.getText().toString();
+        String confpwdVal = confpwd.getText().toString();
+
+        if (nameVal.isEmpty() || roleVal.isEmpty() || usernameVal.isEmpty() || pwdVal.isEmpty() || confpwdVal.isEmpty()){
+            Toast.makeText(createLogin.this, "You are missing some information", Toast.LENGTH_SHORT).show();
+            }
+
+        else if(pwdVal != confpwdVal){
+            Toast.makeText(createLogin.this, "Your Passwords do not match", Toast.LENGTH_SHORT).show();
+            }
     }
 
 

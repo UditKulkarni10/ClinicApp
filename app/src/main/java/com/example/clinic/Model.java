58dafa0d4a3104b1e9d5b6e132fdbd4b77937a3c
rtakwale <55253693+rtakwale@users.noreply.github.com>
2019-11-25 22:54:13 -0500

    Update Model.java

--- app/src/main/java/com/example/clinic/Model.java
+++ app/src/main/java/com/example/clinic/Model.java
@@ -1,18 +1,26 @@
 package com.example.clinic;
 
+import java.util.ArrayList;
+
 public class Model {
     private String title, description;
     private int img;
 
+    public Model(){
+    }
+
     public String getTitle() {
+
         return title;
     }
 
     public void setTitle(String title) {
+
         this.title = title;
     }
 
     public String getDescription() {
+
         return description;
     }
 
@@ -26,5 +34,25 @@ public class Model {
 
     public void setImg(int img) {
         this.img = img;
+    }
+
+    public static ArrayList<Model> initialModels() {
+        ArrayList<Model> list = new ArrayList<>();
+
+        Model model1 = new Model();
+        Model model2 = new Model();
+        Model model3 = new Model();
+        model1.setTitle("Is this working: 1");
+        model1.setDescription("description1");
+        model2.setTitle("Is this not workinggggggg");
+        model2.setDescription("description2");
+        model3.setTitle("This makes me saaaadd");
+        model3.setDescription("description3");
+        list.add(model3);
+        list.add(model1);
+        list.add(model2);
+
+
+        return list;
     }
 }
